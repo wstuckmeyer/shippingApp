34e32009408b30810661e2e0ab54e35166dfc64e
NYCDA One <leightonlee99@gmail.com>
2017-08-22 15:42:31 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20170822181838) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
