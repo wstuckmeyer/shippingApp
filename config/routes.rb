7e0cd0506fca26d129a130244d3d21fd0ffdb589
Wesley Stuckmeyer <wstuckmeyer@gmail.com>
2017-08-22 15:03:36 -0400

    added devise to useres

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+	get 'sessions/new'
 	root to: 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :users, :ships, :jobs, :ship_jobs
+	get    '/login',   to: 'sessions#new'
+  	post   '/login',   to: 'sessions#create'
+  	delete '/logout',  to: 'sessions#destroy'
+
 end
